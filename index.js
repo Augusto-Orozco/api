3c1f28d28f95e04041e441e5f4fc994c6328e631
Augusto <jaugustoorozco06@gmail.com>
2026-05-21 14:45:38 -0600

    bddddd

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const app = express()
 
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://front-end-api-self.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json())
 app.use(morgan("dev"))
 
